021bbde27e6bb21230d0b9afe8383597dfc77fed
nicowiener812 <39963972+nicowiener812@users.noreply.github.com>
2024-03-18 20:17:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/finalassignmentapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.finalassignmentapp;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+ public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+
+        // --------------------- This Part Doesn't Work ---------------------------
+
+
+    }
+}
